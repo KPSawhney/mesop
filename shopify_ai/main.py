62278ffbbdefc6b0f804aa3c2fb349110d8b1dfb
KP Sawhney <kylesawhney@outlook.com>
2024-08-11 16:21:23 -0700

    Fixes

--- shopify_ai/main.py
+++ shopify_ai/main.py
@@ -2,27 +2,17 @@
 import os
 from google import generativeai as genai
 from google.cloud import bigquery
-from google.oauth2 import service_account
 import mesop as me
 import mesop.labs as mel
 from py import gemini_integration
 
-KEY_PATH = "secrets/serviceAccountKey.json"
-
-GCP_CREDS = service_account.Credentials.from_service_account_file(
-    KEY_PATH,
-    scopes=["https://www.googleapis.com/auth/cloud-platform"],
-)
-print('GCP Creds: %s', str(GCP_CREDS))
-
 GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
-BQ_CLIENT = bigquery.Client(
-    project=GCP_PROJECT_ID,
-    credentials=GCP_CREDS)
+BQ_CLIENT = bigquery.Client(project=GCP_PROJECT_ID)
 print('Created BQ client for project %s', BQ_CLIENT.project)
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
+print('Configured Genai with Gemini API key')
 
 
 def transform(q: str, history: list[mel.ChatMessage]):
